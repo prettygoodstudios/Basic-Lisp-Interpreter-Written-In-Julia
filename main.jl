000fae50fec084d4531d1425b94f0711c4472fbf
Miguel Rust <prettygoodstudiosapps@gmail.com>
2022-04-18 22:20:30 -0600

    Got evaluation working with operators.

--- main.jl
+++ main.jl
@@ -44,7 +44,7 @@ function tokenFactory(str::String)::Token
         return StringLiteral(str)
     end
 
-    token = match(r"quote|defun|if", str)
+    token = match(r"quote|defun|if|eq", str)
     if token !== nothing
         return Operator(str)
     end
@@ -79,7 +79,7 @@ end
 Function that returns first found token and program without first token
 """
 function matchToken(program::String)::Tuple{Union{String,Nothing},String}
-    token = match(r"(\w+|\d+|\(|\)|\-|\+|\/|\*|\"(.|\n)*?\")|quote|defun|if",program)
+    token = match(r"(\w+|\d+|\(|\)|\-|\+|\/|\*|\"(.|\n)*?\")|quote|defun|if|eq",program)
     if token === nothing
         return nothing, ""
     end
@@ -132,10 +132,7 @@ end
 """
 Function that prints out parseTree 
 """
-function printParseTree(tree, depth=0)
-    if tree === nothing
-        return
-    end
+function printParseTree(tree::Binding, depth::Int=0)
     tabs = "\t" ^ depth
     println(tabs * "(")
     for token in tree.tokens 
@@ -147,4 +144,73 @@ function printParseTree(tree, depth=0)
         end
     end
     println(tabs * ")")
+end
+
+"""
+Determine if a binding is a function
+"""
+function isFunction(x::Union{Token, Binding})::Bool
+    if typeof(x) === Binding
+        return x.tokens[1] === Operator("defun")
+    end
+    return false
+end
+
+"""
+Determine if a binding is a expression with a operator
+"""
+function isOperatorExpression(x::Union{Token, Binding})
+    if typeof(x) === Binding
+        return x.tokens[1] !== Operator("defun") && typeof(x.tokens[1]) === Operator
+    end
+    return false
+end
+"""
+Evaluates program
+"""
+function evalProgram(tree::Union{Binding,Token})
+    if typeof(tree) === StringLiteral
+        return tree.repr
+    end
+    if typeof(tree) === IntegerLiteral
+        return parse(Int32, tree.repr)
+    end
+    if typeof(tree) === Binding && isFunction(tree)
+        return evalProgram(tree.tokens[4])
+    end
+    if typeof(tree) === Binding && isOperatorExpression(tree)
+        operator = tree.tokens[1].repr
+        if operator === "+"
+            return evalProgram(tree.tokens[2]) + evalProgram(tree.tokens[3])
+        elseif operator === "-"
+            return evalProgram(tree.tokens[2]) - evalProgram(tree.tokens[3])
+        elseif operator === "*"
+            return evalProgram(tree.tokens[2]) * evalProgram(tree.tokens[3])
+        elseif operator === "/"
+            return evalProgram(tree.tokens[2]) / evalProgram(tree.tokens[3])
+        elseif operator === "if"
+            if evalProgram(tree.tokens[2])
+                return evalProgram(tree.tokens[3])
+            elseif count(tree.tokens) === 4
+                return evalProgram(tree.tokens[4])
+            end
+        elseif operator === "eq"
+            return evalProgram(tree.tokens[2]) === evalProgram(tree.tokens[3])
+        end
+    end
+end
+
+"""
+Runs program
+"""
+function runProgram(tree::Binding)
+    # Find functions
+    functions = filter(isFunction, tree.tokens)
+    for f in functions
+        tree.identifiers[f.tokens[1].repr] = f
+    end
+    println(keys(tree.indentifiers))
+    for line in filter(x -> !isFunction(x), tree.tokens)
+        println(evalProgram(line))
+    end
 end
